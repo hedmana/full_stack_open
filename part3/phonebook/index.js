a1735fec509d93992ee8b7dd786ed58b2c0090c0
Axel Hedman <axelhedman00@gmail.com>
2024-05-12 15:26:14 +0300

    3.6 done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,26 +1,28 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+
 let phonebook = [
   {
     id: 1,
     name: "Arto Hellas",
-    number: "040-123456",
+    number: "040-123456"
   },
   {
     id: 2,
     name: "Ada Lovelace",
-    number: "39-44-5323523",
+    number: "39-44-5323523"
   },
   {
     id: 3,
     name: "Dan Abramov",
-    number: "12-43-234345",
+    number: "12-43-234345"
   },
   {
     id: 4,
     name: "Mary Poppendieck",
-    number: "39-23-6423122",
+    number: "39-23-6423122"
   },
 ];
 
@@ -50,12 +52,39 @@ app.get("/info", (request, response) => {
   );
 });
 
+app.post('/api/phonebook', (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  if ((!body.name) || (!body.number)) {
+    return response.status(400).json({ 
+      error: 'missing name or number' 
+    })
+  }
+
+  if (phonebook.some(contact => contact.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    }) 
+  }
+
+  const contact = {
+    id: Math.floor(Math.random() * 100000),
+    name: body.name,
+    number: body.number
+  }
+
+  phonebook = phonebook.concat(contact)
+
+  response.json(contact)
+});
+
 app.delete('/api/phonebook/:id', (request, response) => {
   const id = Number(request.params.id) 
   phonebook = phonebook.filter(note => note.id !== id)
 
   response.status(204).end()
-})
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
